3e4f01d1056fb19db7bfbb57d6680453c643c4a4
Yulong Ruan <yruan@alpha-sense.com>
2021-02-14 12:00:57 +0200

    removed the requires of apollo.config.json

--- apollo.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  client: {
-    service: 'as-schema',
-  },
-};
--- src/loadConfig.ts
+++ src/loadConfig.ts
@@ -1,5 +1,3 @@
-import { cosmiconfig, defaultLoaders } from 'cosmiconfig';
-import TypeScriptLoader from '@endemolshinegroup/cosmiconfig-typescript-loader';
 import path from 'path';
 import fs from 'fs';
 import dotenv from 'dotenv';
@@ -7,19 +5,12 @@ import merge from 'lodash.merge';
 import { CocApolloGraphqlExtensionError } from './errors';
 import { DefaultClientConfig } from './config';
 import { ApolloConfigFormat } from './apollo';
+import { getServiceFromKey } from './utils';
 
 // config settings
-const MODULE_NAME = 'apollo';
-const defaultFileNames = [`${MODULE_NAME}.config.js`, `${MODULE_NAME}.config.ts`];
 const envFileName = '.env';
 const legacyKeyEnvVar = 'ENGINE_API_KEY';
 const keyEnvVar = 'APOLLO_KEY';
-
-const loaders = {
-  '.json': defaultLoaders['.json'],
-  '.js': defaultLoaders['.js'],
-  '.ts': TypeScriptLoader,
-};
 
 interface LoadConfigSettings {
   configPath: string;
@@ -27,18 +18,6 @@ interface LoadConfigSettings {
 }
 
 export async function loadConfig({ configPath }: LoadConfigSettings) {
-  const explorer = cosmiconfig(MODULE_NAME, {
-    searchPlaces: defaultFileNames,
-    loaders,
-  });
-
-  const loadedConfig = await explorer.search(configPath);
-
-  if (!loadedConfig) {
-    throw new CocApolloGraphqlExtensionError(`No config file found ${defaultFileNames}`);
-  }
-
-  let engineConfig = {};
   let apiKey = '';
 
   const dotEnvPath = configPath ? path.resolve(configPath, envFileName) : path.resolve(process.cwd(), envFileName);
@@ -50,22 +29,18 @@ export async function loadConfig({ configPath }: LoadConfigSettings) {
     apiKey = key || legacyKey;
   }
 
-  if (apiKey) {
-    engineConfig = { engine: { apiKey } };
+  if (!apiKey) {
+    throw new CocApolloGraphqlExtensionError('No Apollo API Key fond, make sure it is set in .env');
   }
 
-  if ('service' in loadedConfig?.config) {
-    throw new CocApolloGraphqlExtensionError('Config type `service` is not implemented');
+  const engineConfig = { engine: { apiKey } };
+  const service = getServiceFromKey(apiKey);
+
+  if (!service) {
+    throw new CocApolloGraphqlExtensionError('No Apollo service name found');
   }
 
-  let config: ApolloConfigFormat | null = null;
-  if (loadedConfig.config.client) {
-    config = merge({ client: DefaultClientConfig }, { client: loadedConfig.config.client });
-  }
-
-  if (config && engineConfig) {
-    config = merge(engineConfig, config);
-  }
+  const config: ApolloConfigFormat = merge(engineConfig, { client: DefaultClientConfig }, { client: { service } });
 
   return config;
 }
